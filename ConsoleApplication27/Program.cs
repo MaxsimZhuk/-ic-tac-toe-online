2fac3734e1e7ee75502d5af8b0cf90dab2b5f798
Maxsim Zhuk <maxsimzhuk2011@gmail.com>
2017-02-23 19:45:39 +0200

    add

--- ConsoleApplication27/Program.cs
+++ ConsoleApplication27/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication27.ServiceReference1;
 
 namespace ConsoleApplication27
 {
@@ -10,6 +11,14 @@ namespace ConsoleApplication27
     {
         static void Main(string[] args)
         {
+            using (Service1Client client = new Service1Client())
+            {
+               Console.WriteLine( client.GetSameInt());
+                var data = client.GetData(Guid.NewGuid());
+                Console.WriteLine($"Id: {data}, Name: {data.Name}");
+            }
+            //Та-дам)))) И все работает)
+            Console.ReadKey();
         }
     }
 }
---
+++ WebApplication1/MyClass1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class MyClass1
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        public override string ToString()
+        {
+            return $"Id = {Id}, Name = {Name}";
+        }
+    }
+}
